bf5e69984596d8f7eebdf8173d2b0f8039256666
Daniil1402 <yadanya14@gmail.com>
2021-02-17 23:23:38 +0300

    second version

--- src/components/BlindPrintingSimulator.vue
+++ src/components/BlindPrintingSimulator.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="mainText">
-    <!-- <div class="first__message">Нажмите Enter чтобы начать</div> -->
     <h1>Тренажер слепой печати</h1>
-    <span v-for="(char, idx) of startInfo" :key="idx" :class="{ greenSymb: idx === counterSuccess, redSymb: idx === counterErr }">
+    <span v-for="(char, idx) of text" :key="idx" :class="{ greenSymb: idx === counterSuccess, redSymb: idx === counterErr, passed: idx <= counterSuccess - 1 }">
       {{ char }}
     </span>
     <div class="testTime">Время прохождения теста: {{ time }}</div>
     <div class="speed">Скорость: {{ speed }} зн/мин</div>
     <div class="accuracy">Точность: {{ accuracy }} %</div>
+    <div class="popup" v-if="isEnd">
+      <h3 class="popup__title">Позравляю!!!</h3>
+      <div class="testTime">Время прохождения теста: {{ time }}</div>
+      <div class="speed">Скорость: {{ speed }} зн/мин</div>
+      <div class="accuracy">Точность: {{ accuracy }} %</div>
+      <button v-on:click="reloadPage">Пройти заново</button>
+    </div>
   </div>
 </template>
 
@@ -15,69 +21,88 @@
 export default {
   data() {
     return {
-      startInfo: "",
+      text: "",
       counterSuccess: 0,
       counterErr: null,
-      badKeys: ["Shift", "CapsLock", "Tab", "Control", "Alt", "ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"],
       time: 0,
-      startFlag: false,
-      speed: null,
+      isStart: false,
+      isEnd: false,
+      speed: 0,
       accuracy: 0,
       counter: 0,
     };
   },
   methods: {
-    timeCounting() {
+    updateTime() {
       this.time++;
     },
-    speedCounting() {
-      this.speed = Math.round((this.counterSuccess / this.time) * 60);
+    updateSpeed(counterSuccess, time) {
+      if (!counterSuccess || !time) return;
+      this.speed = Math.round((counterSuccess / time) * 60);
     },
-    accuracyCounting() {
+    updateAccuracy() {
       const acc = (this.counterSuccess * 100) / this.counter;
       this.accuracy = acc.toFixed(1);
     },
     onClickHandler(evt) {
-      const keyFlag = this.badKeys.includes(evt.key);
+      const keyFlag = this.controlKeys.includes(evt.key);
       if (!keyFlag) {
         this.counter++;
-        if (!this.startFlag) {
-          setInterval(this.timeCounting, 1000);
-          setInterval(this.speedCounting, 100);
-          setInterval(this.accuracyCounting, 100);
-          this.startFlag = !this.startFlag;
-        }
-        const isContain = this.startInfo[this.counterSuccess].includes(evt.key);
-        this.startFlag = true;
+        const isContain = this.text[this.counterSuccess].includes(evt.key);
         if (isContain) {
           this.counterSuccess++;
           if (this.counterErr) {
             this.counterErr = null;
+          } else if (this.counterSuccess == this.text.length) {
+            this.stopUpdate();
           }
         } else {
           this.counterErr = this.counterSuccess;
         }
+        if (!this.isStart) {
+          this.startUpdate();
+        }
       }
+    },
+    startUpdate() {
+      this.timeInterval = setInterval(() => this.updateTime(), 1000);
+      this.speedInterval = setInterval(() => this.updateSpeed(this.counterSuccess, this.time), 100);
+      this.accuracyInterval = setInterval(() => this.updateAccuracy(), 100);
+      this.isStart = !this.isStart;
+    },
+    stopUpdate() {
+      clearInterval(this.timeInterval);
+      clearInterval(this.speedInterval);
+      clearInterval(this.accuracyInterval);
+      document.removeEventListener("keydown", this.onClickHandler);
+      // console.log("THE END");
+      this.isEnd = true;
+    },
+    reloadPage() {
+      window.location.reload();
     },
   },
   async mounted() {
     try {
       let response = await fetch("https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1");
       response = await response.json();
-      this.startInfo = response[0].split("");
+      //response = ["aaa aaaaa aaaa aa"];
+      this.text = response[0].split("");
     } catch (error) {
       console.error(error.message);
     }
 
     document.addEventListener("keydown", this.onClickHandler);
   },
+  created() {
+    this.controlKeys = ["Shift", "CapsLock", "Tab", "Control", "Alt", "ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"];
+  },
 };
 </script>
 
 <style scoped>
 .mainText {
-  /* max-width: 680px;
-  width: 100%; */
+  width: 100%;
   height: 225px;
   overflow: hidden;
   font-size: 21px;
@@ -86,22 +111,71 @@ export default {
 
 span {
   min-width: 7px;
-  min-height: 12px;
-  display: inline-block;
+  line-height: 1.5;
+  display: inline-flex;
+  position: relative;
 }
 
-.greenSymb {
+span::after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  border-radius: 3px;
+}
+
+.greenSymb::after {
   background: rgba(19, 221, 19, 0.781);
 }
 
-.redSymb {
+.redSymb::after {
   background: red;
 }
 
 .greenSymb,
 .redSymb {
-  padding: 3px;
-  border-radius: 3px;
+  padding: 0 3px;
   color: #fff;
+  min-width: 7px;
+  min-height: 10px;
+  color: white;
+}
+
+.passed {
+  color: rgb(55, 194, 62);
+}
+
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.popup > div {
+  padding: 10px;
+  background: #fff;
+  color: #000;
+  max-width: 400px;
+  width: 100%;
+}
+
+.popup h3 {
+  padding: 10px;
+  background: #fff;
+  max-width: 400px;
+  width: 100%;
+  margin: 0;
 }
 </style>
